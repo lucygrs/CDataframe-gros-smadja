867d7e4dd7a01203ac5f8acfdec8dec887120b90
Arthur S <arsmadja92@gmail.com>
2024-03-28 15:12:40 +0100

    Improved column struct (on failed to create)

--- Column.c
+++ Column.c
@@ -3,9 +3,12 @@
 //
 
 #include "Column.h"
+#include <stdio.h>
 
 COLUMN *create_column(char* title){
     COLUMN* column = malloc(1*sizeof(COLUMN));
+    if(!column)
+        printf("Couldn't create column");
 
     for(int i = 0; i<NAME_SIZE && title[i]!='\0'; i++){
         column->_name[i] = title[i];
